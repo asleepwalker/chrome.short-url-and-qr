924baa4492358b83072b12523da613990ef7475a
asleepwalker <mail@asleepwalker.ru>
2014-02-03 23:49:16 +0200

    Made it more understandable

--- src/js/generator.js
+++ src/js/generator.js
@@ -1,17 +1,18 @@
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-	var longUrl = tabs[0].url;
+	var longUrl = tabs[0].url; // from active tab
 	var xmlhttp = new XMLHttpRequest();
-	var url = 'https://www.googleapis.com/urlshortener/v1/url';
-	var data = '{"longUrl":"'+longUrl+'"}';
+	var url = 'https://www.googleapis.com/urlshortener/v1/url'; // goo.gl API
+	var data = '{"longUrl":"'+longUrl+'"}'; // JSON
 	xmlhttp.open('POST', url, true);
 	xmlhttp.setRequestHeader('Content-type', 'application/json');
 	xmlhttp.send(data);
 
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+	xmlhttp.onreadystatechange = function() { // waiting for short link
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { // success
 			var response = JSON.parse(xmlhttp.responseText);
 			document.getElementById('url').value = response.id;
-			new QRCode(document.getElementById('qr'), { text: response.id, width: 128, height: 128 });
+			new QRCode(document.getElementById('qr'), { text: response.id, width: 128, height: 128 }); // generating QR by qrcode.js
+			document.getElementById('qr').style.borderColor = '#fff';
 		}
 	}
 });
