c7500d84ea539f63b1a62fe5f8fea4d837149b1c
asleepwalker <mail@asleepwalker.ru>
2014-09-21 13:23:02 +0300

    Removed uninformative comments and styled code

--- src/js/generator.js
+++ src/js/generator.js
@@ -2,18 +2,23 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 	var longUrl = tabs[0].url; // from active tab
 	var xmlhttp = new XMLHttpRequest();
 	var url = 'https://www.googleapis.com/urlshortener/v1/url'; // goo.gl API
-	var data = '{"longUrl":"'+longUrl+'"}'; // JSON
+	var data = '{"longUrl":"'+longUrl+'"}';
 	xmlhttp.open('POST', url, true);
 	xmlhttp.setRequestHeader('Content-type', 'application/json');
 	xmlhttp.send(data);
 
-	xmlhttp.onreadystatechange = function() { // waiting for short link
+	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { // success
 			var response = JSON.parse(xmlhttp.responseText);
 			document.getElementById('url').value = response.id;
-			new QRCode(document.getElementById('qr'), { text: response.id, width: 128, height: 128 }); // generating QR by qrcode.js
-			document.getElementById('qr').className = 'done'; // remove frame and loader
+			new QRCode(document.getElementById('qr'), {
+				text: response.id,
+				width: 128,
+				height: 128
+			});
+			document.getElementById('qr').className = 'done';
 			document.getElementById('url').removeAttribute('readonly');
-		} else document.getElementById('qr').className = 'error'; // smth went wrong
-	}
+		}
+		else document.getElementById('qr').className = 'error';
+	};
 });
