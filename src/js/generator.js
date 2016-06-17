ad895f7cc5e5b6223ff35cb2b262facd5579abdb
Sleepwalker <mail@asleepwalker.ru>
2016-06-17 19:33:45 +0300

    Add permanent readonly input, update dates, clean and restyle source code

--- src/js/generator.js
+++ src/js/generator.js
@@ -1,30 +1,32 @@
 /*
-*	Short URL and QR-code generator for Google Chrome, v1.2
-*	(c) 2014–2015 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
+*	Short URL and QR-code generator for Google Chrome, v1.3
+*	(c) 2014–2016 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
 *	https://github.com/asleepwalker/chrome.short-url-and-qr
 */
 
-chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
 	var longUrl = tabs[0].url; // from active tab
 	var xmlhttp = new XMLHttpRequest();
 	var url = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyCJc796c9LKjvtqIOmE7C06VW30yvfawQc';
-	var data = '{"longUrl":"'+longUrl+'"}';
+	var data = '{"longUrl":"' + longUrl + '"}';
 	xmlhttp.open('POST', url, true);
 	xmlhttp.setRequestHeader('Content-type', 'application/json');
 	xmlhttp.send(data);
 
 	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { // success
+		var qr = document.getElementById('qr');
+		var input = document.getElementById('url');
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			var response = JSON.parse(xmlhttp.responseText);
-			document.getElementById('url').value = response.id;
-			new QRCode(document.getElementById('qr'), {
+			input.value = response.id;
+			new QRCode(qr, {
 				text: response.id,
 				width: 128,
 				height: 128
 			});
-			document.getElementById('qr').className = 'done';
-			document.getElementById('url').removeAttribute('readonly');
+			qr.className = 'done';
+		} else {
+			qr.className = 'error';
 		}
-		else document.getElementById('qr').className = 'error';
 	};
 });
