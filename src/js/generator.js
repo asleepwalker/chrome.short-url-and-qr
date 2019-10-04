08030b9441561184aaca3d2125a823cb1c01899e
Artyom Fedosov <mail@asleepwalker.ru>
2019-10-03 19:47:54 -0600

    Switch to tinyurl.com

--- src/js/generator.js
+++ src/js/generator.js
@@ -1,43 +1,49 @@
 /*
-*	Short URL and QR-code generator for Google Chrome, v1.3
-*	(c) 2014–2017 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
-*	https://github.com/asleepwalker/chrome.short-url-and-qr
+* Short URL and QR-code generator for Google Chrome, v1.4
+* (c) 2014–2019 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
+* https://github.com/asleepwalker/chrome.short-url-and-qr
 */
 
 chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-	var longUrl = tabs[0].url; // from active tab
-	var xmlhttp = new XMLHttpRequest();
-	var url = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyCJc796c9LKjvtqIOmE7C06VW30yvfawQc';
-	var data = '{"longUrl":"' + longUrl + '"}';
-	xmlhttp.open('POST', url, true);
-	xmlhttp.setRequestHeader('Content-type', 'application/json');
-	xmlhttp.send(data);
-
-	xmlhttp.onreadystatechange = function() {
-		var qr = document.getElementById('qr');
-		var input = document.getElementById('url');
-		var msg = document.getElementById('msg');
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			var response = JSON.parse(xmlhttp.responseText);
-			input.value = response.id;
-			new QRCode(qr, {
-				text: response.id,
-				width: 128,
-				height: 128
-			});
-			qr.className = 'done';
-
-			input.parentElement.onclick = function() {
-				input.select();
-				if (document.execCommand('copy')) {
-					msg.className = 'visible';
-					setTimeout(function() {
-						msg.className = '';
-					}, 2000);
-				}
-			};
-		} else {
-			qr.className = 'error';
-		}
-	};
+  const longUrl = tabs[0].url; // from active tab
+  shortenUrl(longUrl)
+    .then(showResult)
+    .catch(showError);
 });
+
+async function shortenUrl(longUrl) {
+  if (!/^https?:\/\//.test(longUrl)) {
+    throw new Error('Invalid input URL');
+  }
+
+  const response = await fetch(`https://tinyurl.com/api-create.php?url=${longUrl}`);
+  return response.text();
+}
+
+function showResult(shortUrl) {
+  const qr = document.getElementById('qr');
+  const input = document.getElementById('url');
+  const msg = document.getElementById('msg');
+
+  input.value = shortUrl;
+  new QRCode(qr, {
+    text: shortUrl,
+    width: 128,
+    height: 128
+  });
+  qr.className = 'done';
+
+  // Copy to clipboard on input click
+  input.parentElement.onclick = function() {
+    input.select();
+    if (document.execCommand('copy')) {
+      msg.className = 'visible';
+      setTimeout(() => { msg.className = '';  }, 2000);
+    }
+  }
+}
+
+function showError() {
+  const qr = document.getElementById('qr');
+  qr.className = 'error';
+}
